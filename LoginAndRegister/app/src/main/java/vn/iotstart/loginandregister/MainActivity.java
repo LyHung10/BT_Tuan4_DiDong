5ed110e4c0235d9e282d631ab7e8a6ecec1750bd
LyHung10 <lyhung10nctlop95@gmail.com>
2025-02-21 14:01:10 +0700

    complete

--- LoginAndRegister/app/src/main/java/vn/iotstart/loginandregister/MainActivity.java
+++ LoginAndRegister/app/src/main/java/vn/iotstart/loginandregister/MainActivity.java
@@ -1,6 +1,9 @@
 package vn.iotstart.loginandregister;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView textViewRegister = findViewById(R.id.textView2);
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
